2d683579bb0fa5921e377128eda6b0b8b84c3bd6
William <willspri@gmail.com>
2024-02-20 12:22:13 +1300

    first

---
+++ values.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  // Assign value 15 to myNum
+  int myNum = 15;
+
+  // Declare valueless myOtherNum
+  int myOtherNum;
+
+  // Assign value of myNum to myOtherNum
+  myOtherNum = myNum;
+
+  // myOtherNum now value 15
+  printf("%d", myOtherNum);
+
+  return 0;
+}
